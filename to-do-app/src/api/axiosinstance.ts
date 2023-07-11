cc4699c4640b25ac1797d018e409e9e8d25ade29
Bianca Raetchi <bianca.raetchi@gmail.com>
2023-07-11 18:13:46 +0300

    Configured node to recognize the .ts extension

--- to-do-app/src/api/axiosinstance.ts
+++ to-do-app/src/api/axiosinstance.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 const baseURL = process.env.NETLIFY ? "https://json-server-todo-6885f432f23b.herokuapp.com/" : "http://localhost:4545/"
+console.log(process.env.NETLIFY)
+console.log(baseURL)
+
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
