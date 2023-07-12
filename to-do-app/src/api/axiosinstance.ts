32a0c64c2a14166b270628ebeb477295bccf754e
Bianca Raetchi <bianca.raetchi@gmail.com>
2023-07-12 18:42:15 +0300

    Changed server address

--- to-do-app/src/api/axiosinstance.ts
+++ to-do-app/src/api/axiosinstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_DEPLOYED==='true' ? "https://json-server-todo-6885f432f23b.herokuapp.com/" : "http://localhost:4545/"
+const baseURL = import.meta.env.VITE_DEPLOYED==='true' ? "https://json-server-todo-9ac24788cd96.herokuapp.com/" : "http://localhost:4545/"
 console.log(import.meta.env.VITE_DEPLOYED)
 console.log(baseURL)
 
