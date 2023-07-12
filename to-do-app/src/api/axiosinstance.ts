d52301c3a5c6445ab2032698eb28c45781adf257
Bianca Raetchi <bianca.raetchi@gmail.com>
2023-07-12 12:13:41 +0300

    Added it to satisfy the vite specific way

--- to-do-app/src/api/axiosinstance.ts
+++ to-do-app/src/api/axiosinstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_NETLIFY_LOCAL==='true' ? "https://json-server-todo-6885f432f23b.herokuapp.com/" : "http://localhost:4545/"
-console.log(process.env.NEXT_PUBLIC_NETLIFY_LOCAL)
+const baseURL = import.meta.env.NEXT_PUBLIC_NETLIFY_LOCAL==='true' ? "https://json-server-todo-6885f432f23b.herokuapp.com/" : "http://localhost:4545/"
+console.log(import.meta.env.NEXT_PUBLIC_NETLIFY_LOCAL)
 console.log(baseURL)
 
 
